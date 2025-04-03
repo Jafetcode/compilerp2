544a3b1ad68d6dc2a768c04c608381cc4b1ad06f
cloax <147194673+cl0ax@users.noreply.github.com>
2025-04-03 10:02:02 -0500

    Update SynAnalyzer.cpp

--- SynAnalyzer.cpp
+++ SynAnalyzer.cpp
@@ -1,7 +1,3 @@
-//
-// Created by Abraham Hernandez on 4/1/25.
-//
-
 #include "SynAnalyzer.h"
 #include <iostream>
 #include <map>
@@ -15,83 +11,138 @@ SyntaxAnalyzer::SyntaxAnalyzer(istream &infile) {
     while (getline(infile, wordPair)) {
         unsigned int pos = wordPair.find(" : ");
         string token = wordPair.substr(0, pos);
-        string lex = wordPair.substr(pos + 1, wordPair.size());
+        string lex = wordPair.substr(pos + 3, wordPair.size());
         lexemes.push_back(lex);
         tokens.push_back(token);
     }
+    tokitr = tokens.begin();
+    lexitr = lexemes.begin();
 }
 
 bool SyntaxAnalyzer::parse() {
-    if (tokitr != tokens.end() && *tokitr  == "t_main") {
-        ++tokitr; ++lexitr;
-        if (tokitr!=tokens.end() && *tokitr  == "s_lbrace") {
-            tokitr++; lexitr++;
+    if (tokitr != tokens.end() && *tokitr == "t_main") {
+        ++tokitr;
+        ++lexitr;
+        if (tokitr != tokens.end() && *tokitr == "s_lbrace") {
+            tokitr++;
+            lexitr++;
             if (stmtlist()) {
-                if (tokitr!=tokens.end() && *tokitr  == "s_rbrace") {
-                    tokitr++; lexitr++;
+                if (tokitr != tokens.end() && *tokitr == "s_rbrace") {
+                    tokitr++;
+                    lexitr++;
                     if (tokitr == tokens.end()) {
                         return true;
                     }
                 }
-
             }
         }
     }
     cout << *tokitr << " " << *lexitr << endl;
     return false;
 }
+
 // Start of Abraham's Section
-bool SyntaxAnalyzer::stmtlist() { // STMTLIST → STMT [STMT]m | Ø
+bool SyntaxAnalyzer::stmtlist() {
+    // STMTLIST → STMT [STMT]m | Ø
     int result = stmt();
-    while (result != -1) {
+    while (result != -1 && result != 0) {
         result = stmt();
     }
-    return true;
+    if (result == 0) {
+        return false;
+    } else {
+        return true;
+    }
 }
+
+bool SyntaxAnalyzer::vdecassign(string test) {
+    // TYPE id = EXPR;
+    if (tokitr != tokens.end()) {
+        if (*tokitr == "t_integer" || *tokitr == "t_string") {
+            string test = *lexitr;
+            ++tokitr;
+            ++lexitr;
+            if (tokitr != tokens.end() && *tokitr == "t_id") {
+                // makes only one variable be in the list at once.
+                if (!symboltable.contains(*lexitr)) {
+                    symboltable.insert(make_pair(*lexitr, test));
+                }
+                ++tokitr;
+                ++lexitr;
+
+                if (tokitr != tokens.end() && *tokitr == "s_assign") {
+                    ++tokitr;
+                    ++lexitr;
+                    if (expr()) {
+                        // expr should move up after it is done
+                        if (tokitr != tokens.end() && *tokitr == "s_semi") {
+                            ++tokitr;
+                            ++lexitr;
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;
+} // jacob
+
 int SyntaxAnalyzer::stmt() {
-    if (tokitr != lexemes.end() && *tokitr == "t_if") {
-        tokitr++; lexitr++;
-        if (!ifstmt()){ //check if it is a boolean since wwe are returning integer
+    if (*tokitr == "t_integer" || *tokitr == "t_string") {
+        string test = *lexitr;
+        ++tokitr;
+        ++lexitr;
+        if (!vdecassign(test)) {
+            return 0; //why int and not bool,
+        } else {
+            return 1;
+        }
+    }
+    if (tokitr != tokens.end() && *tokitr == "t_if") {
+        tokitr++;
+        lexitr++;
+        if (!ifstmt()) {
+            //check if it is a boolean since wwe are returning integer
             //checking if they are true
             return 0; //0 means this statement isn't valid/does not apply here
         } else {
             return 1; //1 means valid
         }
     }
-    if (tokitr != lexemes.end() && *tokitr == "t_while") {
-        tokitr++; lexitr++;
+    if (tokitr != tokens.end() && *tokitr == "t_while") {
+        ++tokitr;
+        ++lexitr;
         if (!whilestmt()) {
             return 0;
-        }
-        else {
+        } else {
             return 1;
         }
     }
-    if (tokitr != lexemes.end() && *tokitr == "t_assign") {
-        tokitr++; lexitr++; //start with an id
+    if (tokitr != tokens.end() && *tokitr == "t_assign") {
+        tokitr++;
+        lexitr++; //start with an id
         if (!assignstmt()) {
             return 0;
-        }
-        else {
+        } else {
             return 1;
         }
-
     }
-    if (tokitr != lexemes.end() && *tokitr == "t_input") {
-        tokitr++; lexitr++;
+    if (tokitr != tokens.end() && *tokitr == "t_input") {
+        tokitr++;
+        lexitr++;
         if (!inputstmt()) {
             return 0;
-        }
-        else {
+        } else {
             return 1;
-        }
+        } // fix  to tokens
     }
-    if (tokitr != lexemes.end() && *tokitr == "t_output") {
-        tokitr++; lexitr++;
+    if (tokitr != tokens.end() && *tokitr == "t_output") {
+        tokitr++;
+        lexitr++;
         if (!outputstmt()) {
             return 0;
-        }
-        else {
+        } else {
             return 1;
         }
     }
@@ -100,35 +151,43 @@ int SyntaxAnalyzer::stmt() {
 
 bool SyntaxAnalyzer::term() {
     // TERM -> number | text | id | (EXPR)
-    if (tokitr != lexemes.end()) {
+    if (tokitr != tokens.end()) {
         //this checks if token is num, text, or id
-        if (*tokitr == "t_number" || *tokitr == "t_text" || *tokitr == "t_id") {
-            ++tokitr; ++lexitr;
+        if (*tokitr == "t_number" || *tokitr == "t_text") {
+            ++tokitr;
+            ++lexitr;
+            return true;
+        }
+        if (*tokitr == "t_id" && !symboltable.contains(*lexitr)) {
+            ++tokitr;
+            ++lexitr;
             return true;
         }
         //this will check for EXPR
         else if (*tokitr == "s_lparen") {
-            ++tokitr; ++lexitr; //this will eat '(' (i think lol)
+            ++tokitr;
+            ++lexitr; //this will eat '(' (i think lol)
             if (expr()) {
-                if (expr()) {
-                    if (tokitr != lexemes.end() && *tokitr == "s_rparen") {
-                        ++tokitr; ++lexitr; // should eat ')'
-                        return true;
-                    } else {
-                        cout << "error in source code" << endl;
-                        cout << "missing ')' in term" << endl;
-                        return false;
-                    }
+                if (tokitr != tokens.end() && *tokitr == "s_rparen") {
+                    ++tokitr;
+                    ++lexitr; // should eat ')'
+                    return true;
                 } else {
+                    cout << "error in source code" << endl;
+                    cout << "missing ')' in term" << endl;
                     return false;
                 }
+            } else {
+                return false;
             }
         }
         return false;
     }
 }
-bool SyntaxAnalyzer::arithop() { // ARITHOP -> s_plus | s_minus | s_mult | s_div | s_mod
-    if (tokitr != lexemes.end()) {
+
+bool SyntaxAnalyzer::arithop() {
+    // ARITHOP -> s_plus | s_minus | s_mult | s_div | s_mod
+    if (tokitr != tokens.end()) {
         if (*tokitr == "s_plus" || *tokitr == "s_minus" || *tokitr == "s_div") {
             return true;
         }
@@ -136,11 +195,13 @@ bool SyntaxAnalyzer::arithop() { // ARITHOP -> s_plus | s_minus | s_mult | s_div
     return false;
 }
 
-bool SyntaxAnaluzer::expr() { //EXPR -> SIMPLEEXPR  [LOGICOP  SIMPLEEXPR]1
+bool SyntaxAnalyzer::expr() {
+    //EXPR -> SIMPLEEXPR  [LOGICOP  SIMPLEEXPR]1
     if (simpleexpr()) {
-        if (tokir != lexeme.end()) {
+        if (tokitr != tokens.end()) {
             if (*tokitr == "t_and" || *tokitr == "t_or") {
-                ++tokitr: ++lexitr;
+                ++tokitr;
+                ++lexitr;
                 if (!simpleexpr()) {
                     cout << "error in source code" << endl;
                     cout << "simple expression after logical op is needed" << endl;
@@ -152,23 +213,31 @@ bool SyntaxAnaluzer::expr() { //EXPR -> SIMPLEEXPR  [LOGICOP  SIMPLEEXPR]1
     }
     return false;
 }
+
 // End of Abraham's Section
 
 bool SyntaxAnalyzer::whilestmt() {
     if (tokitr != lexemes.end() && *tokitr == "t_while") {
-        ++tokitr;++lexitr;
+        ++tokitr;
+        ++lexitr;
         if (tokitr != lexemes.end() && *tokitr == "s_lparen") {
-            ++tokitr;++lexitr;
+            ++tokitr;
+            ++lexitr;
             if (tokitr != lexemes.end() && expr()) {
-                ++tokitr;++lexitr;
+                ++tokitr;
+                ++lexitr;
                 if (tokitr != lexemes.end() && *tokitr == "s_rparen") {
-                    ++tokitr;++lexitr;
+                    ++tokitr;
+                    ++lexitr;
                     if (tokitr != lexemes.end && *tokitr == "s_lbrace") {
-                        ++tokitr;++lexitr;
+                        ++tokitr;
+                        ++lexitr;
                         if (tokitr != lexemes.end() && stmtlist()) {
-                            ++tokitr;++lexitr;
+                            ++tokitr;
+                            ++lexitr;
                             if (tokitr != lexemes.end() && *tokitr == "s_rbrace") {
-                                ++tokitr;++lexitr;
+                                ++tokitr;
+                                ++lexitr;
                                 return true;
                             } else {
                                 cout << "error in source code" << endl;
@@ -196,13 +265,17 @@ bool SyntaxAnalyzer::whilestmt() {
 
 bool SyntaxAnalyzer::inputstmt() {
     if (tokitr != lexemes.end() && *tokitr == "t_input") {
-        ++tokitr;++lexitr;
+        ++tokitr;
+        ++lexitr;
         if (tokitr != lexemes.end() && *tokitr == "s_lparen") {
-            ++tokitr;++lexitr;
+            ++tokitr;
+            ++lexitr;
             if (tokitr != lexemes.end() && *tokitr == "t_id") {
-                ++tokitr;++lexitr;
+                ++tokitr;
+                ++lexitr;
                 if (tokitr != lexemes.end() && *tokitr == "s_rparen") {
-                    ++tokitr;++lexitr;
+                    ++tokitr;
+                    ++lexitr;
                     if (tokitr == lexemes.end()) {
                         return true;
                     }
@@ -215,24 +288,32 @@ bool SyntaxAnalyzer::inputstmt() {
 
 bool SyntaxAnalyzer::outputstmt() {
     if (tokitr != lexemes.end() && *tokitr == "t_output") {
-        ++tokitr;++lexitr;
+        ++tokitr;
+        ++lexitr;
         if (tokitr != lexemes.end() && *tokitr == "s_lparen") {
-            ++tokitr;++lexitr;
+            ++tokitr;
+            ++lexitr;
             if (tokitr != lexemes.end() && expr()) {
-                ++tokitr;++lexitr;
+                ++tokitr;
+                ++lexitr;
                 if (tokitr != lexemes.end() && *tokitr == "s_rparen") {
-                    ++tokitr;++lexitr;
+                    ++tokitr;
+                    ++lexitr;
                     if (tokitr == tokens.end()) {
-                        tokitr--;tokitr--;
+                        tokitr--;
+                        tokitr--;
                     }
                     return true;
                 }
             } else if (tokitr != lexemes.end() && *tokitr == "t_text") {
-                ++tokitr;++lexitr;
+                ++tokitr;
+                ++lexitr;
                 if (tokitr != lexemes.end() && *tokitr == "s_lparen") {
-                    ++tokitr;++lexitr;
+                    ++tokitr;
+                    ++lexitr;
                     if (tokitr == tokens.end()) {
-                        tokitr--;tokitr--;
+                        tokitr--;
+                        tokitr--;
                     }
                     return true;
                 }
@@ -244,22 +325,29 @@ bool SyntaxAnalyzer::outputstmt() {
 
 bool SyntaxAnalyzer::simpleexpr() {
     if (tokitr != lexemes.end() && term()) {
-        ++tokitr;++lexitr;
+        ++tokitr;
+        ++lexitr;
         if (arithop()) {
-            ++tokitr;++lexitr;
+            ++tokitr;
+            ++lexitr;
             if (tokitr != lexemes.end() && term()) {
-                ++tokitr;++lexitr;
+                ++tokitr;
+                ++lexitr;
                 if (tokitr == tokens.end()) {
-                    --tokitr;--lexitr;
+                    --tokitr;
+                    --lexitr;
                 }
                 return true;
             }
         } else if (relop()) {
-            ++tokitr;++lexitr;
+            ++tokitr;
+            ++lexitr;
             if (tokitr != lexemes.end() && term()) {
-                ++tokitr;++lexitr;
+                ++tokitr;
+                ++lexitr;
                 if (tokitr == tokens.end()) {
-                    --tokitr;--lexitr;
+                    --tokitr;
+                    --lexitr;
                 }
                 return true;
             }
@@ -292,51 +380,32 @@ bool logicop(vector<string> lexemes, vector<string> tokens, vector<string>::iter
         return false;
     }
 }
-// jacob section
-bool SyntaxAnalyzer::vdecassign() {
-    // TYPE id = EXPR;
-    if (tokitr != tokens.end()) {
-        if (*tokitr == "t_integer" || *tokitr == "t_string") {
-            string test = *lexitr;
-            ++tokitr; ++lexitr;
-            if (tokitr != tokens.end() && *tokitr == "t_id") {
-                // makes only one variable be in the list at once.
-                if (!symboltable.contains(*lexitr)) {
-                    symboltable.insert(make_pair(*lexitr, test));
-                }
-                ++tokitr; ++lexitr;
 
-                if (tokitr != tokens.end() && *tokitr == "s_assign") {
-                    ++tokitr; ++lexitr;
-                    if (expr()) { // expr should move up after it is done
-                        if (tokitr != tokens.end() && *tokitr == "s_semi") {
-                            ++tokitr; ++lexitr;
-                            return true;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return false;
-} // jacob
+// jacob section
+
 bool SyntaxAnalyzer::ifstmt() {
     // if (EXPR) { STMTLIST } ELSEPART
     if (tokitr != tokens.end()) {
         if (*tokitr == "t_if") {
-            ++tokitr; ++lexitr;
+            ++tokitr;
+            ++lexitr;
             if (tokitr != tokens.end() && *tokitr == "s_lparen") {
-                ++tokitr; ++lexitr;
+                ++tokitr;
+                ++lexitr;
                 if (expr()) {
                     if (tokitr != tokens.end() && *tokitr == "s_rparen") {
-                        ++tokitr; ++lexitr;
+                        ++tokitr;
+                        ++lexitr;
                         if (tokitr != tokens.end() && *tokitr == "s_lbrace") {
-                            ++tokitr; ++lexitr;
+                            ++tokitr;
+                            ++lexitr;
                             if (stmtlist()) {
                                 if (tokitr != tokens.end() && *tokitr == "s_rbrace") {
-                                    ++tokitr; ++lexitr;
+                                    ++tokitr;
+                                    ++lexitr;
                                     if (elsepart()) {
-                                        ++tokitr; ++lexitr;
+                                        ++tokitr;
+                                        ++lexitr;
                                         return true;
                                     }
                                 }
@@ -352,12 +421,15 @@ bool SyntaxAnalyzer::ifstmt() {
 bool SyntaxAnalyzer::elsepart() {
     // else { STMTLIST } | null
     if (tokitr != tokens.end() && *tokitr == "t_else") {
-        ++tokitr; ++lexitr;
+        ++tokitr;
+        ++lexitr;
         if (tokitr != tokens.end() && *tokitr == "s_lbrace") {
-            ++tokitr; ++lexitr;
+            ++tokitr;
+            ++lexitr;
             if (stmtlist()) {
                 if (tokitr != tokens.end() && *tokitr == "s_rbrace") {
-                    ++tokitr; ++lexitr;
+                    ++tokitr;
+                    ++lexitr;
                     return true;
                 }
             }
@@ -371,10 +443,12 @@ bool SyntaxAnalyzer::assignstmt() {
     if (tokitr != tokens.end()) {
         if (*tokitr == "t_id" && symboltable.contains(*lexitr)) {
             if (tokitr != tokens.end() && *tokitr == "s_assign") {
-                ++tokitr; ++lexitr;
+                ++tokitr;
+                ++lexitr;
                 if (expr()) {
                     if (tokitr != tokens.end() && *tokitr == "s_semi") {
-                        ++tokitr; ++lexitr;
+                        ++tokitr;
+                        ++lexitr;
                         return true;
                     }
                 }
@@ -385,14 +459,15 @@ bool SyntaxAnalyzer::assignstmt() {
 } // jacob
 bool SyntaxAnalyzer::relop() {
     if (tokitr != tokens.end()) {
-        if (*tokitr == "s_lt" || *tokitr == "s_gt" || *tokitr == "s_eq" ||*tokitr == "s_ne") {
-            ++tokitr; ++lexitr;
+        if (*tokitr == "s_lt" || *tokitr == "s_gt" || *tokitr == "s_eq" || *tokitr == "s_ne") {
+            ++tokitr;
+            ++lexitr;
             return true;
         }
     }
     return false;
 } // jacob
-bool SyntaxAnalyzer::typecheck(const string& type1) {
+bool SyntaxAnalyzer::typecheck(const string &type1) {
     if (type1 == symboltable[*lexitr]) {
         return true;
     }
