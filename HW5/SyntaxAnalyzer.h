53b6ea4394fa1fd19a58592d93ac8e39369f7f23
Jacob Luytjes <jluytjes01@aurora.edu>
2025-04-03 11:05:29 -0500

    Update SyntaxAnalyzer.h

--- HW5/SyntaxAnalyzer.h
+++ HW5/SyntaxAnalyzer.h
@@ -1,48 +1,54 @@
-
+//
+// Created by jacob on 3/28/2025.
+//
 
 #ifndef SYNTAXANALYZER_H
 #define SYNTAXANALYZER_H
 
-
-
-#include <iostream>
-#include <vector>
+#include <fstream>
 #include <map>
-#include <string>
-
+#include <vector>
 using namespace std;
 
-class SyntaxAnalyzer {
+class SyntaxAnalyzer{
 private:
     vector<string> lexemes;
     vector<string> tokens;
     vector<string>::iterator lexitr;
     vector<string>::iterator tokitr;
+    // map of variables and their datatype
+    // i.e. sum t_integer
+    map<string, string> symboltable; // add var declearation to this
 
-    map<string, string> symboltable; // map of variables and their datatype
-
-    // Private methods
-    bool vdec();
-    int vars();
-    bool stmtlist();
-    int stmt();
-    bool ifstmt();
-    bool elsepart();
-    bool whilestmt();
-    bool assignstmt();
-    bool inputstmt();
-    bool outputstmt();
-    bool expr();
-    bool simpleexpr();
-    bool term();
-    bool logicop();
-    bool arithop();
-    bool relop();
+    // other private methods
+    bool vdecassign(string test); // jacob
+    bool stmtlist(); // abraham
+    int stmt(); // abraham
+    bool ifstmt(); // jacob
+    bool elsepart(); // jacob
+    bool whilestmt(); // jafet
+    bool assignstmt(); // jacob
+    bool inputstmt(); // jafet
+    bool outputstmt(); // jafet
+    bool expr(); // abraham
+    bool simpleexpr(); // jafet
+    bool term(); // abraham
+    bool logicop(); // jafet
+    bool arithop(); // abraham
+    bool relop(); // jacob
 
 public:
     SyntaxAnalyzer(istream& infile);
+    // pre: 1st parameter consists of an open file containing a source code's
+    //	valid scanner/lexical analyzer output.  This data must be in the form: token : lexeme
+    // post: the vectors have been populated
 
     bool parse();
+    // pre: none
+    // post: The lexemes/tokens have been parsed.
+    // If an error occurs, a message prints indicating the token/lexeme pair
+    // that caused the error.
+    // If no error, vectors contain syntactically correct source code
 };
 
-#endif
+#endif //SYNTAXANALYZER_H
